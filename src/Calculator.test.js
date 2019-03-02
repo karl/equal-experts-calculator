a6aec4193145b2a26437dfd73ff02224d733ac89
Karl O'Keeffe <github@monket.net>
2019-03-02 21:40:19 +0000

    Refactor tests to make them quicker to write

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -3,23 +3,58 @@ import { render, fireEvent, cleanup } from 'react-testing-library';
 import { Calculator } from './Calculator';
 import 'jest-dom/extend-expect';
 
+const getHelpers = (getByText, getByTestId) => {
+  return {
+    press: (key) => fireEvent.click(getByText(key)),
+    expectResult: (result) =>
+      expect(getByTestId('display')).toHaveTextContent(
+        new RegExp(`^${result}$`),
+      ),
+  };
+};
+
 describe('Calculator', () => {
+  let press, expectResult;
+  beforeEach(() => {
+    const { getByText, getByTestId } = render(<Calculator />);
+    ({ press, expectResult } = getHelpers(getByText, getByTestId));
+  });
+
   afterEach(cleanup);
 
   it('starts with a display of 0', () => {
-    const { getByTestId } = render(<Calculator />);
-    expect(getByTestId('display')).toHaveTextContent('0');
+    expectResult(0);
   });
 
   it('can add two single digit numbers', () => {
-    const { getByText, getByTestId } = render(<Calculator />);
-    expect(getByTestId('display')).toHaveTextContent('0');
+    press('4');
+    press('+');
+    press('3');
+    press('=');
+    expectResult('7');
+  });
 
-    fireEvent.click(getByText('4'));
-    fireEvent.click(getByText('+'));
-    fireEvent.click(getByText('3'));
-    fireEvent.click(getByText('='));
+  it('can subtract two single digit numbers', () => {
+    press('4');
+    press('-');
+    press('3');
+    press('=');
+    expectResult('1');
+  });
 
-    expect(getByTestId('display')).toHaveTextContent('7');
+  it('can multiply two single digit numbers', () => {
+    press('4');
+    press('*');
+    press('3');
+    press('=');
+    expectResult('12');
+  });
+
+  it('can divide two single digit numbers', () => {
+    press('6');
+    press('/');
+    press('2');
+    press('=');
+    expectResult('3');
   });
 });
